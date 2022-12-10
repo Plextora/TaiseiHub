d8cc63a0d0dbde3fcfcee48fb4c33da521383a2b
Plextora <71889427+Plextora@users.noreply.github.com>
2022-12-09 20:07:17 -0500

    Added game status label

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿// Copyright Plextora 2022
 // This file is licensed under GPL-3.0-or-later
 
+using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Memory;
 
 namespace TaiseiHub
 {
@@ -10,9 +15,47 @@ namespace TaiseiHub
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer _checkGameState = new DispatcherTimer();
+        static Mem _memoryManager = new Mem();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _checkGameState.Tick += _checkGameState_Tick;
+            _checkGameState.Interval = new TimeSpan(0, 0, 1);
+            _checkGameState.Start();
+
+            if (!_memoryManager.OpenProcess("taisei")) SetStatusLabel("Closed!", "#DC143C");
+        }
+
+        private void _checkGameState_Tick(object sender, EventArgs e)
+        {
+            int pausedState = _memoryManager.ReadInt("taisei.exe+98E3D8");
+
+            if (pausedState != 0) SetStatusLabel("Playing!", "#98FB98");
+            else SetStatusLabel("Paused/In Menu!", "#F0E68C");
+
+            if (!IsTaiseiRunning()) SetStatusLabel("Closed!", "#DC143C");
+            else if (IsTaiseiRunning()) _memoryManager.OpenProcess("taisei");
+        }
+
+        public static bool IsTaiseiRunning()
+        {
+            Process[] name = Process.GetProcessesByName("taisei");
+
+            if (name.Length == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SetStatusLabel(string status, string colorHexCode)
+        {
+            StatusLabel.Foreground = new BrushConverter().ConvertFromString($"{colorHexCode}") as Brush;
+            StatusLabel.Content = $"Game Status: {status}";
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
