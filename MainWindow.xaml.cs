7caa617ab4295f44e7845603e6b1bf412832761d
Plextora <71889427+Plextora@users.noreply.github.com>
2022-12-10 21:17:53 -0500

    Added Infinite Health cheat

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
@@ -16,6 +17,7 @@ namespace TaiseiHub
     public partial class MainWindow : Window
     {
         DispatcherTimer _checkGameState = new DispatcherTimer();
+        DispatcherTimer _giveInfHealth = new DispatcherTimer();
         static Mem _memoryManager = new Mem();
 
         public MainWindow()
@@ -24,23 +26,59 @@ namespace TaiseiHub
 
             _checkGameState.Tick += _checkGameState_Tick;
             _checkGameState.Interval = new TimeSpan(0, 0, 1);
+
+            _giveInfHealth.Tick += _giveInfHealth_Tick;
+            _giveInfHealth.Interval = new TimeSpan(0, 0, (int)0.5);
             _checkGameState.Start();
 
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
 
-            if (!_memoryManager.OpenProcess("taisei")) SetStatusLabel("Closed!", "#DC143C");
+            if (!_memoryManager.OpenProcess("taisei")) SetStatusLabel(GameStatusLabel, "Game Status: Closed!", "#DC143C");
         }
+
+        #region Cheats
+
+        private void InfiniteHealthButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (IsTaiseiRunning())
+            {
+                if (_giveInfHealth.IsEnabled)
+                {
+                    InfiniteHealthButton.Content = "Infinite Health (Off)";
+                    SetStatusLabel(CheatStatusLabel, "Cheat Status: No cheats running", "#ffdab9");
+                    _giveInfHealth.Stop();
+                    _memoryManager.WriteMemory("taisei.exe+98F548", "int", "2");
+                    return;
+                }
+
+                SetStatusLabel(CheatStatusLabel, "Cheat Status: Running infinite health!", "#c7fcc7");
+                InfiniteHealthButton.Content = "Infinite Health (On)";
+
+                _giveInfHealth.Start();
+            }
+        }
+
+        private void _giveInfHealth_Tick(object sender, EventArgs e)
+        {
+            if (!_memoryManager.WriteMemory("taisei.exe+98F548", "int", "8"))
+            {
+                SetStatusLabel(CheatStatusLabel, "Cheat status: Failed to write memory.", "#DC143C");
+                InfiniteHealthButton.Content = "Infinite Health (Off)";
+            }
+        }
+
+        #endregion
 
         private void _checkGameState_Tick(object sender, EventArgs e)
         {
             int pausedState = _memoryManager.ReadInt("taisei.exe+977B50");
             int pausedStateExtended = _memoryManager.ReadInt("taisei.exe+98E3D8");
 
-            if (pausedState == 0) SetStatusLabel("Paused!", "#F0E68C");
-            else if (pausedStateExtended != 0) SetStatusLabel("Playing!", "#98FB98");
-            else SetStatusLabel("In Menu!", "#F0E68C");
+            if (pausedState == 0) SetStatusLabel(GameStatusLabel, "Game Status: Paused!", "#F0E68C");
+            else if (pausedStateExtended != 0) SetStatusLabel(GameStatusLabel, "Game Status: Playing!", "#98FB98");
+            else SetStatusLabel(GameStatusLabel, "Game Status: In Menu!", "#F0E68C");
 
-            if (!IsTaiseiRunning()) SetStatusLabel("Closed!", "#DC143C");
+            if (!IsTaiseiRunning()) SetStatusLabel(GameStatusLabel, "Game Status: Closed!", "#DC143C");
             else if (IsTaiseiRunning()) _memoryManager.OpenProcess("taisei");
         }
 
@@ -56,10 +94,10 @@ namespace TaiseiHub
             return true;
         }
 
-        private void SetStatusLabel(string status, string colorHexCode)
+        private void SetStatusLabel(System.Windows.Controls.Label statusLabel, string statusText, string colorHexCode)
         {
-            StatusLabel.Foreground = new BrushConverter().ConvertFromString($"{colorHexCode}") as Brush;
-            StatusLabel.Content = $"Game Status: {status}";
+            statusLabel.Foreground = new BrushConverter().ConvertFromString($"{colorHexCode}") as Brush;
+            statusLabel.Content = $"{statusText}";
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
